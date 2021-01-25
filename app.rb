32ee2278e975b7d39cf812e55b6bfec7ed5eb897
Olek <forgemest@gmail.com>
2021-01-26 00:02:59 +0200

    Keep barber choice on postback

--- app.rb
+++ app.rb
@@ -61,26 +61,26 @@ post '/contacts' do
   @mail = params[:mail]
   @body = params[:body]
 
-    require 'pony'
-    Pony.mail(
-        :name => params[:name],
-        :mail => params[:mail],
-        :body => params[:body],
-        :to => 'forgemest@gmail.com',
-        :subject => params[:name] + " has contacted you",
-        :body => params[:message],
-        :port => '587',
-        :via => :smtp,
-        :via_options => {
-            :address              => 'smtp.gmail.com',
-            :port                 => '587',
-            :enable_starttls_auto => true,
-            :user_name            => 'lumbee',
-            :password             => 'p@55w0rd',
-            :authentication       => :plain,
-            :domain               => 'localhost.localdomain'
-        })
-    redirect '/success'
+    # require 'pony'
+    # Pony.mail(
+    #     :name => params[:name],
+    #     :mail => params[:mail],
+    #     :body => params[:body],
+    #     :to => 'forgemest@gmail.com',
+    #     :subject => params[:name] + " has contacted you",
+    #     :body => params[:message],
+    #     :port => '587',
+    #     :via => :smtp,
+    #     :via_options => {
+    #         :address              => 'smtp.gmail.com',
+    #         :port                 => '587',
+    #         :enable_starttls_auto => true,
+    #         :user_name            => 'lumbee',
+    #         :password             => 'p@55w0rd',
+    #         :authentication       => :plain,
+    #         :domain               => 'localhost.localdomain'
+    #     })
+    # redirect '/success'
 
 
   # Вывод ошибки
