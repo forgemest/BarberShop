4c63a54c480fbb87b03fbbccb6aa33fbff3c4f45
Olek <forgemest@gmail.com>
2021-02-10 00:14:47 +0200

    Remove hardcoded barbers list

--- app.rb
+++ app.rb
@@ -2,10 +2,28 @@ require 'rubygems'
 require 'sinatra'
 require 'sqlite3'
 
+def is_barber_exists? db, name
+  db.execute('select * from Barbers where name=?', [name]).length > 0
+end
+
+def seed_db db, barbers
+  barbers.each do |barber|
+    if !is_barber_exists? db, barber
+      db.execute 'insert into Barbers (name) values (?)', [barber]
+    end
+  end
+end
+
 def get_db
   db = SQLite3::Database.new 'barbershop.db'
   db.results_as_hash = true
   return db
+end
+
+before do
+  db = get_db
+  @barbers = db.execute 'select * from Barbers'
+
 end
 
 configure do
@@ -20,6 +38,15 @@ configure do
       "barber" TEXT,
       "color" TEXT
   )'
+
+  db.execute 'CREATE TABLE IF NOT EXISTS
+  "Barbers"
+  (
+      "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+      "name" TEXT
+  )'
+
+  seed_db db, ['Jessie Pinkman', 'Walter White', 'Gus Fring', 'Mike Ehrmantraut']
 end
 
 get '/' do
@@ -84,3 +111,8 @@ post '/contacts' do
 end
 
 
+get '/showusers' do
+  db = get_db
+  @results = db.execute 'select * from Users order by id desc'
+  erb :showusers
+  end
